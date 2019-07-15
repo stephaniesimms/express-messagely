5b46215906403ea577186872de7e560a2a3c9db0
Stephanie Simms <stephanie.simms@gmail.com>
2019-07-15 12:32:29 -0700

    added user class methods to register, authenticate, updatelogintimestamp, get all users, get specific user

--- models/user.js
+++ models/user.js
@@ -1,29 +1,86 @@
 /** User class for message.ly */
-
+const db = require("../db");
+const ExpressError = require("../expressError");
+const bcrypt = require("bcrypt");
 
 
 /** User of the site. */
 
 class User {
-
+  constructor({ username, password, first_name, last_name, phone }) {
+    this.username = username;
+    this.password = password;
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.phone = phone;
+  }
   /** register new user -- returns
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { }
+  static async register({ username, password, first_name, last_name, phone }) {
+    
+    const result = await db.query(
+      `INSERT INTO users (
+        username, 
+        password,
+        first_name,
+        last_name,
+        phone,
+        join_at)
+      VALUES ($1, $2, $3, $4, $5, current_timestamp)
+      RETURNING username, password, first_name, last_name, phone`,
+      [username, password, first_name, last_name, phone]);
+
+    return result.rows[0];
+  }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
 
-  static async authenticate(username, password) { }
+  static async authenticate(username, password) {
+    // Query for user
+    const result = await db.query(
+      `SELECT password
+      FROM users
+      WHERE username=$1`,
+      [username]
+    );
+    const user = result.rows[0];
+
+    if (!user) {
+      return false;
+    } else {
+      if (await bcrypt.compare(password, user.password) === true) {
+        return true;
+      }
+    }      
+  }
 
   /** Update last_login_at for user */
 
-  static async updateLoginTimestamp(username) { }
+  static async updateLoginTimestamp(username) {
+    const result = await db.query(
+      `UPDATE users
+      SET last_login_at=current_timestamp
+      WHERE username=$1
+      RETURNING username, last_login_at`,
+      [username]
+    );
+
+    return result.rows[0];
+  }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
 
-  static async all() { }
+  static async all() {
+    const results = await db.query(
+      `SELECT username, first_name, last_name, phone 
+      FROM users`
+    );
+
+    return results.rows.map(user => new User(user));
+  }
 
   /** Get: get user by username
    *
@@ -34,7 +91,22 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) { }
+  static async get(username) { 
+    const results = await db.query(
+      `SELECT username, 
+          password,
+          first_name,
+          last_name,
+          phone,
+          join_at,
+          last_login_at
+        FROM users
+        WHERE username = $1
+        RETURNING username, password, first_name, last_name, phone, join_at, last_login_at`,
+        [username]);
+      
+    return results.rows[0];
+  }
 
   /** Return messages from this user.
    *
@@ -44,7 +116,41 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesFrom(username) { }
+  static async messagesFrom(username) { 
+    const results = await db.query(
+      `SELECT id, to_username, body, sent_at, read_at
+        FROM messages
+        WHERE from_username = $1`,
+      [username]
+    );
+
+    return  { id, 
+      to_user: {
+        username, 
+        first_name, 
+        last_name, 
+        phone
+      }, 
+      body, sent_at, read_at }
+    }
+
+    // return results.rows.map(row => )
+
+  }
+
+  const results = await db.query(
+    `SELECT id, 
+           customer_id AS "customerId", 
+           num_guests AS "numGuests", 
+           start_at AS "startAt", 
+           notes AS "notes"
+         FROM reservations 
+         WHERE customer_id = $1`,
+    [customerId]
+  );
+
+    return results.rows.map(row => new Reservation(row));
+  }
 
   /** Return messages to this user.
    *
@@ -59,3 +165,28 @@ class User {
 
 
 module.exports = User;
+
+
+// INSERT INTO users(
+//   username,
+//   password,
+//   first_name,
+//   last_name,
+//   phone,
+//   join_at)
+// VALUES('Stephanie',
+//  'shhh',
+// 'Stephanie',
+//   'Simms',
+// '666-666-6666',
+//  current_timestamp);
+
+// INSERT INTO messages(
+//   from_username,
+//   to_username,
+//   body,
+//   sent_at)
+//  VALUES('Stephanie',
+//   'Hekmat',
+//   'AHOY!',
+//   current_timestamp);
